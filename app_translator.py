613a530015fdb34db61fb07cd7d23f2086ba03aa
Bhawna-Bhoria <145017122+Bhawna-Bhoria@users.noreply.github.com>
2024-04-27 00:40:51 +0530

    Update app_translator.py

--- app_translator.py
+++ app_translator.py
@@ -1,35 +1,38 @@
 import streamlit as st
 from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 
-st.title("Hindi Grammar Error Correction App")
-
-# Load MBart model and tokenizer
-model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
-tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
-
 def translate_hindi_to_french(input_text):
+    model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+    tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+
     # Set source language to Hindi
     tokenizer.src_lang = "hi_IN"
     encoded_input = tokenizer(input_text, return_tensors="pt")
-    
+
     # Generate translation
     generated_tokens = model.generate(
         **encoded_input,
         forced_bos_token_id=tokenizer.lang_code_to_id["hi_IN"]  # Target language French
     )
-    
+
     # Decode and return translated text
     translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
     return translated_text
 
-# Input text area
-input_text = st.text_area("Enter text in Hindi:", height=200)
+def main():
+    st.title("Hindi to French Translator")
 
-# Translate button
-if st.button("Translate"):
-    if input_text:
-        translated_text = translate_hindi_to_french(input_text)
-        st.success("Translated Text (French):")
-        st.text(translated_text)
-    else:
-        st.warning("Please enter some hindi text to correct it.")
+    # Input text area
+    input_text = st.text_area("Enter text in Hindi:", height=200)
+
+    # Translate button
+    if st.button("Translate"):
+        if input_text:
+            translated_text = translate_hindi_to_french(input_text)
+            st.success("Translated Text (French):")
+            st.text(translated_text)
+        else:
+            st.warning("Please enter some text to translate.")
+
+if __name__ == "__main__":
+    main()
